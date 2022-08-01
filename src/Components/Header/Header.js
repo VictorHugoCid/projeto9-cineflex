54ff03971a9f1a034b980242a5454576c35122a0
VictorCid <victorcidpereira@gmail.com>
2022-08-01 08:33:50 -0300

    july 08h30mim cleaning

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router-dom'
 import './Header.css'
 
 export default function Header(){
 
+    const navigate = useNavigate()
     return (
 
-        <div className='header'>
-            CINEFLEX
+        <div className='header' onClick={() => {navigate('/')}}>
+            <h1>CINEFLEX</h1>
         </div>
     )
 }
